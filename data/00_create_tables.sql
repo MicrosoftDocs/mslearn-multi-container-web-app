7d67a6f26fbca08158bfc212fb842a7b0733b18b
Sidney Andrews <v-sandrews@microsoft.com>
2021-06-08 13:23:59 -0400

    Initial commit

---
+++ data/00_create_tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE locations(
+    id                      VARCHAR(100),
+    name                    VARCHAR(100),
+    longitude               DOUBLE,
+    latitude                DOUBLE,
+    mailingAddress          VARCHAR(100),
+    parkingIncluded         BOOLEAN,
+    conferenceRoomsIncluded BOOLEAN,
+    receptionIncluded       BOOLEAN,
+    publicAccess            BOOLEAN,
+    lastRenovationDate      DATETIME,
+    image                   VARCHAR(100)
+);
+
+CREATE TABLE locationrooms(
+    description       VARCHAR(100),
+    monthlyRate       DOUBLE,
+    seats             INT,
+    privateFacilities BOOLEAN,
+    phoneIncluded     BOOLEAN,
+    windows           BOOLEAN,
+    corner            BOOLEAN
+);
